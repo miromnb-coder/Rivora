f3503ed6d01d4fc6267ee9e1b063c70eb26f88e4
miromnb-coder <miromnb@icloud.com>
2026-09-24 21:39:33 +0300

    Build Rivora marketing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import { redirect } from "next/navigation";
+import { MarketingNav } from "@/components/marketing/MarketingNav";
+import { MarketingFooter } from "@/components/marketing/MarketingFooter";
+import {
+  HeroSection,
+  LightDarkTransition,
+  HowItWorks,
+  ProblemSolution,
+  CustomerMemory,
+  ConfidenceSystem,
+  AiExtraction,
+  FinalCta,
+} from "@/components/marketing/MarketingSections";
 
 export default function Home() {
-  redirect("/app/inbox");
+  return (
+    <main className="marketing-page">
+      <div className="marketing-shell">
+        <MarketingNav />
+      </div>
+      <HeroSection />
+      <LightDarkTransition />
+      <HowItWorks />
+      <ProblemSolution />
+      <CustomerMemory />
+      <ConfidenceSystem />
+      <AiExtraction />
+      <FinalCta />
+      <MarketingFooter />
+    </main>
+  );
 }
