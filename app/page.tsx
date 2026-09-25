b7c5a1d16ed7f3aab8a5ab42561b96f905a1f09a
miromnb-coder <miromnb@icloud.com>
2026-09-25 21:13:43 +0300

    Pass locale to marketing homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { MarketingNav } from "@/components/marketing/MarketingNav";
 import { MarketingFooter } from "@/components/marketing/MarketingFooter";
 import { MinimalShaderHero } from "@/components/marketing/MinimalShaderHero";
 import { PricingLeadCapture } from "@/components/marketing/PricingLeadCapture";
+import { getLocale } from "@/lib/locale";
 import {
   ProofStripV2,
   HowItWorksV2,
@@ -13,23 +14,25 @@ import {
   FinalCtaV2,
 } from "@/components/marketing/HomepageV2Sections";
 
-export default function Home() {
+export default async function Home() {
+  const locale = await getLocale();
+
   return (
     <main className="marketing-page minimal-direction">
       <div className="marketing-shell">
-        <MarketingNav />
+        <MarketingNav locale={locale} />
       </div>
-      <MinimalShaderHero />
-      <ProofStripV2 />
-      <HowItWorksV2 />
-      <ProductResolutionV2 />
-      <CustomerMemoryV2 />
-      <ConfidenceSystemV2 />
-      <AiExtractionV2 />
-      <BeforeAfterV2 />
-      <PricingLeadCapture />
-      <FinalCtaV2 />
-      <MarketingFooter />
+      <MinimalShaderHero locale={locale} />
+      <ProofStripV2 locale={locale} />
+      <HowItWorksV2 locale={locale} />
+      <ProductResolutionV2 locale={locale} />
+      <CustomerMemoryV2 locale={locale} />
+      <ConfidenceSystemV2 locale={locale} />
+      <AiExtractionV2 locale={locale} />
+      <BeforeAfterV2 locale={locale} />
+      <PricingLeadCapture locale={locale} />
+      <FinalCtaV2 locale={locale} />
+      <MarketingFooter locale={locale} />
     </main>
   );
 }
