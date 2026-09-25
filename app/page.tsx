907d5a8ed74aac498780bc494af6e959accb996b
miromnb-coder <miromnb@icloud.com>
2026-09-25 08:55:10 +0300

    Use homepage v2 workflow and confidence sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import { MarketingNav } from "@/components/marketing/MarketingNav";
 import { MarketingFooter } from "@/components/marketing/MarketingFooter";
 import { MinimalShaderHero } from "@/components/marketing/MinimalShaderHero";
 import {
+  HowItWorksV2,
   ProductResolutionV2,
   CustomerMemoryV2,
+  ConfidenceSystemV2,
 } from "@/components/marketing/HomepageV2Sections";
 import {
-  HowItWorks,
-  ConfidenceSystem,
   AiExtraction,
   FinalCta,
 } from "@/components/marketing/MarketingSections";
@@ -19,10 +19,10 @@ export default function Home() {
         <MarketingNav />
       </div>
       <MinimalShaderHero />
-      <HowItWorks />
+      <HowItWorksV2 />
       <ProductResolutionV2 />
       <CustomerMemoryV2 />
-      <ConfidenceSystem />
+      <ConfidenceSystemV2 />
       <AiExtraction />
       <FinalCta />
       <MarketingFooter />
